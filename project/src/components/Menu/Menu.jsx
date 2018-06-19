ce313c1f45646e628174ea97529e0e70628f6c87
Vladislav Gorbachev <Gorbachev.WEB@gmail.com>
2018-06-19 16:18:24 +0300

    hw#04 added

--- project/src/components/Menu/Menu.jsx
+++ project/src/components/Menu/Menu.jsx
@@ -1,14 +1,36 @@
 import './Menu.css';
+
 import React, { Component } from 'react';
 
 export default class Menu extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            activeTab: 0
+        }
+    }
+
+    setActiveTab = (e) =>{
+        this.setState({
+            activeTab: Array.prototype.indexOf.call(e.target.parentNode.children, e.target)
+        });
+
+        e.preventDefault();
+    }
+
     render(){
         const { items } = this.props;
         return (
-            <div className="menu">
-                <ul>
-                    {items.map(item => <li><a href={item.href}>{item.title}</a></li>)}
-                </ul>
+            <div className="menu-wrapper">
+                <div className="menu">
+                    {items.map((item, idx) => {
+                        if(idx == this.state.activeTab){
+                            return <a href={item.href} className="active" onClick={this.setActiveTab}>{item.title}</a>;
+                        }
+                        return <a href={item.href} onClick={this.setActiveTab}>{item.title}</a>;
+                    })}
+                </div>
             </div>
         );
     }
